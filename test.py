c2cd89b40e98d9ebd93e2cf55c44ad9c206a92cc
MrAndMrswang <chaowang93@163.com>
2022-04-11 19:36:19 +0800

    use test for test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-from decode import SpacePacketDecoder
+from spacePacket.spacePacketCreator import SpacePacketCreator
 
 
 def ReadFile():
@@ -9,10 +9,13 @@ def ReadFile():
     # IE Huffmann BRC = 000
     binFile = open(filePath, 'rb')
     size = os.path.getsize(filePath)
-    for i in range(90):
-        data = binFile.read(1)
-        if i < 5:
-            print("data:", data)
+    data = binFile.read(10)
+    print("data:", data, " data[0]:", data[0:1])
+    # for i in range(90):
+    #     data = binFile.read(10)
+    #     data[0]
+    #     if i < 5:
+    #         print("data:", data)
         # os.sys.stdout.buffer.write(data)
         # r_int = int.from_bytes(data, byteorder='big')  #将 byte转化为 int
         # b = '{:08b}'.format(r_int)
@@ -26,15 +29,7 @@ def ReadFile():
 if __name__ == "__main__":
 
     filePath = '/home/chao/work/NNSFC/Program/RawDataProcessor/data/S1A_IW_RAW__0SDV_20220315T061928_20220315T062001_042328_050BC0_43C6.SAFE/s1a-iw-raw-s-vh-20220315t061928-20220315t062001-042328-050bc0.dat'
-    decoder = SpacePacketDecoder(filePath)
-    res = decoder.sampleReconstruction(2, 239, 1, 5)
-    print("res:", res)
-
-    res = decoder.sampleReconstruction(3, 3, -1, 9)
-    print("res:", res)
-
-    res = decoder.sampleReconstruction(3, 5, -1, 9)
-    print("res:", res)
+    creator = SpacePacketCreator(filePath)
+    creator.createSpacekets()
 
 
-    # ReadFile()
