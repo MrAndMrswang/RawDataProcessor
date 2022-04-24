8edcdd474b49564720f85a4c4bf2c4e91489b5cf
MrAndMrswang <chaowang93@163.com>
2022-04-24 16:23:13 +0800

    [fix] make log change one hour

--- utils/log.py
+++ utils/log.py
@@ -11,46 +11,29 @@ if not log0.exists():
     os.mkdir(log0)
 
 
-def makeLogger(moduleName):
-    date = time.strftime('%Y%m%d%H', time.localtime(time.time()))
-    fileName = moduleName + '_' + date + '.txt'
+def makeLogger(fileName):
     #创建日志级别
-    logger = logging.getLogger(moduleName)
+    logger = logging.getLogger(fileName)
     logger.setLevel(logging.INFO)
 
     #定义日志格式
     formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(filename)s|%(lineno)d|%(message)s')
 
     # 
-    th = handlers.TimedRotatingFileHandler(logDir+fileName, when='H', encoding='utf-8')
+    th = handlers.TimedRotatingFileHandler(logDir+fileName+".txt", when='H', encoding='utf-8')
     th.setFormatter(formatter)
 
     #添加到日志记录器中
     logger.addHandler(th)
     logger.info("init")
 
-    _logDict.update({moduleName: {'logFileName':moduleName, 'log':logger}})
+    _logDict.update({fileName: {'logFileName':fileName, 'log':logger}})
 
 
 def getLogger(moduleName):
-    if not _logDict.__contains__(moduleName):
-        makeLogger(moduleName)
+    date = time.strftime('%Y%m%d%H', time.localtime(time.time()))
+    fileName = moduleName + '_' + date
+    if not _logDict.__contains__(fileName):
+        makeLogger(fileName)
 
-    return _logDict[moduleName]['log']
-
-
-if __name__ == "__main__":
-    log = getLogger("default")
-    log.info("test")
-
-
-    log = getLogger("ddd")
-    log.info("test")
-
-    log = getLogger("default")
-    log.info("test")
-
-
-    log = getLogger("ddd")
-    log.info("test")
-
+    return _logDict[fileName]['log']
